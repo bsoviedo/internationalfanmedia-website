808dbe400c8c75cb306dc9f874e9ed1b8c28926c
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-07-24 20:11:09 -0400

    Added website icon

--- app/Components/Body.tsx
+++ app/Components/Body.tsx
@@ -1,8 +1,10 @@
 
 export default function Body() {
     return (
-    <main className="h-screen pt-12.5 relative bg-gradient-to-r from-blue-400 to-purple-500 flex justify-center items-center">
-        <h1 className="font-bold text-white text-4xl">Website is Currently Under Construction</h1>
+    <main className="h-screen pt-12.5 relative bg-gradient-to-r from-blue-400 to-purple-500 flex justify-center items-center ">
+        <h1 className="font-bold text-white text-4xl motion-safe:animate-bounce hover:text-orange-500">
+            Website is Currently Under Construction
+        </h1>
       </main>
     )
 }
