33526da54c84ef0b50d8c2e280fe27a6e0253c53
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-07-28 20:07:08 -0400

    Added Construction Page

---
+++ app/Components-Home/ConstructionPage.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+export default function Construction() {
+    return (
+    <main className='h-screen '>
+        <div className='pt-12.5  bg-black justify-center items-center relative flex pt-20 '>
+            <h1 className="font-bold text-white text-4xl hover:text-orange-500 ">
+                Website is Currently Under Construction
+            </h1>  
+        </div>
+        <div className="pt-12.5 relative bg-black justify-center items-center relative flex pt-20 " >
+            <h2 className="font-bold text-white text-4xl ">
+                <Link href="/remin">Visit Remin</Link>
+            </h2>
+        </div>
+        <div className="pt-12.5 relative bg-black justify-center items-center relative flex pt-20 " >
+            <h2 className="font-bold text-white text-4xl ">
+                <Link href="/barcelona">Visit Barcelona</Link>
+            </h2>
+        </div>
+        <div className="pt-12.5 relative bg-black justify-center items-center relative flex pt-20 " >
+            <h2 className="font-bold text-white text-4xl ">
+                <Link href="/valencia">Visit Valencia</Link>
+            </h2>
+        </div>
+    </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from './Components-Home/Header'
 import Body from './Components-Home/Body'
 import Footer from './Components-Home/Footer'
-
+import Construction from './Components-Home/ConstructionPage'
 
 export default function Page() {
   return(
         <div>
           <Header/>
-          <Body/>
+          <Construction/>
+          {/* <Body/> */}
           <Footer/>
         </div>
       )
