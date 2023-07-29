c56d7644988c89bdad262aa2ae8d06bdb1603e82
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-07-28 20:03:59 -0400

    Added Linker to link remin website

--- app/Components-Home/Body.tsx
+++ app/Components-Home/Body.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
+
 export default function Body() {
     return (
-    <main className="h-screen pt-12.5 relative bg-black flex justify-center items-center ">
-        <h1 className="font-bold text-white text-4xl motion-safe:animate-bounce hover:text-orange-500">
-            Website is Currently Under Construction
-        </h1>    
+    <main className='h-screen '>
+        <div className='pt-12.5  bg-black justify-center items-center relative flex pt-20 '>
+            <h1 className="font-bold text-white text-4xl hover:text-orange-500 ">
+                Website is Currently Under Construction
+            </h1>  
+        </div>
+        <div className="pt-12.5 relative bg-black justify-center items-center relative flex pt-20 " >
+            <h2 className="font-bold text-white text-4xl ">
+                <Link href="/remin">Visit Remin</Link>
+            </h2>
+        </div>
     </main>
     )
 }
