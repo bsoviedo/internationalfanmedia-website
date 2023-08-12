227713dc2359797070f5ba22c5d234ea3bc6650c
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-08-12 00:55:19 -0400

    Updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import TopHeader from './Components-Home/Top-Header'
 import Header from './Components-Home/Header'
-// import Body from './Components-Home/Body'
+import Body from './Components-Home/Body'
 import Footer from './Components-Home/Footer'
 import Construction from './Components-Home/ConstructionPage'
-import AboutUs from './Components-Home/AboutUs'
-import ContactUs from './Components-Home/Contact'
-import Careers from './Components-Home/Careers'
+// import AboutUs from './Components-Home/AboutUs'
+// import ContactUs from './Components-Home/Contact'
+// import Careers from './Components-Home/Careers'
 
 export default function Page() {
   return(
         <div>
           <TopHeader/>
           <Header/>
-          <Construction/>
-          {/* <Body/> */}
+          {/* <Construction/> */}
+          <Body/>
           {/* <AboutUs/>
           <ContactUs/>
           <Careers/> */}
