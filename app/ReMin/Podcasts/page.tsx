e50785d2b0ebe678ef848d7b6ec0e833f48546b9
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-08-14 22:43:46 -0400

    Updated podcasts page

--- app/ReMin/Podcasts/page.tsx
+++ app/ReMin/Podcasts/page.tsx
@@ -2,17 +2,14 @@ import TopHeader from '../Components-Remin/Top-Header'
 import Header from '../Components-Remin/Header'
 import Construction from '../Components-Remin/Construction'
 import Footer from '../Components-Remin/Footer'
+import Body from './podcast'
 
 export default function Podcasts() {
     return(
         <div>
             <TopHeader/>
             <Header/>
-            <div className="h-screen pt-12.5 relative bg-gradient-to-r from-amber-300 to-blue-600 to-purple-500 flex justify-center items-center ">
-                <h1 className="font-bold text-white text-4xl animate-fade animate-ease-in animate-once ">
-                        Podcasts is Currently Under Construction
-                    </h1>    
-                </div>
+            <Body/>
             <Footer/>
         </div>
     )
---
+++ app/ReMin/Podcasts/podcast.tsx
@@ -0,0 +1,73 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+
+const Body: React.FC = () => {
+    // States to manage visibility
+    const [recentUploadsVisible, setRecentUploadsVisible] = useState<boolean>(false);
+    const [shortsVisible, setShortsVisible] = useState<boolean>(false);
+    const [divider1Visible, setDivider1Visible] = useState<boolean>(false);
+    const [divider2Visible, setDivider2Visible] = useState<boolean>(false);
+
+    
+
+    // Refs to track divs
+    const recentUploadsRef = useRef<HTMLDivElement | null>(null);
+    const shortsRef = useRef<HTMLDivElement | null>(null);
+    const divider1Ref = useRef<HTMLDivElement | null>(null);
+    const divider2Ref = useRef<HTMLDivElement | null>(null);
+
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.target === recentUploadsRef.current && entry.isIntersecting) {
+                    setRecentUploadsVisible(true);
+                } else if (entry.target === shortsRef.current && entry.isIntersecting) {
+                    setShortsVisible(true);
+                }
+            });
+        }, {
+            rootMargin: '0px',
+            threshold: 0.1
+        });
+
+        if (recentUploadsRef.current) {
+            observer.observe(recentUploadsRef.current);
+        }
+
+        if (shortsRef.current) {
+            observer.observe(shortsRef.current);
+        }
+
+        // Cleanup observer on component unmount
+        return () => {
+            if (recentUploadsRef.current) observer.unobserve(recentUploadsRef.current);
+            if (shortsRef.current) observer.unobserve(shortsRef.current);
+        };
+    }, []);
+
+    return (
+            <div className="bg-gradient-to-b from-fuchsia-300 to-purple-400 overflow-y-auto p-5 h-screen">
+                <div className="mx-auto max-w-screen-xl px-4 lg:px-0 pt-5">  
+                    <div className="mt-8" ref={recentUploadsRef}>
+                        <a href="https://www.youtube.com/@ReMinFanMedia/videos" target="_blank" rel="noopener noreferrer" className='hover:underline'>
+                                <h1 className={`text-white text-3xl font-bold mb-8 ${recentUploadsVisible ? 'animate-fade-right animate-once animate-ease-in-out' : ''}`}>Podcasts</h1>
+                            </a>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                    {[
+                                        { thumbnail: '/thumbnail-6.png', videoId: 'IEUjvBagcwo'}
+                                    ].map((item, idx) => (
+                                        <a href={`https://youtu.be/${item.videoId}`} key={idx} target="_blank" rel="noopener noreferrer" className="relative">
+                                            <div className="aspect-w-16 aspect-h-9">
+                                                <img src={item.thumbnail} alt={`Vid-${idx + 1}`} className={`object-cover w-full h-full rounded-lg hover:scale-105 transition duration-150 ease-in-out shadow-lg ${recentUploadsVisible ? 'animate-fade animate-once animate-ease-in' : ''}`} />
+                                            </div>
+                                        </a>
+                                    ))}
+                            </div>
+                    </div>   
+                </div>
+            </div>
+        );
+}
+
+export default Body;
