edcf88198d22e46437df4c5715d2fbd6ed75aab2
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2023-09-01 17:00:05 -0400

    Update

--- app/Components-Home/AboutUs.tsx
+++ app/Components-Home/AboutUs.tsx
@@ -6,13 +6,13 @@ import Link from 'next/link'
 export default function AboutUs() {
    
     return (
-     <div className="bg-slate-900 dark:bg-gray-900 ">
+     <div className="bg-slate-200 dark:bg-gray-900 ">
         <div className=" h-screen py-8 px-4 mx-auto max-w-screen-xl lg:py-16  lg:px-6 ">
             <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white dark:text-white">About Us</h2>
-                <p className="font-light text-white lg:mb-16 sm:text-xl dark:text-gray-400">International Fan Media Description</p>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Us</h2>
+                <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">International Fan Media Description</p>
             </div> 
-            <div className="grid gap-8 mb-6 lg:mb-16 md:grid-rows-2">
+            <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
                 <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="/Picture1.png" alt="Bonnie Avatar"/>
