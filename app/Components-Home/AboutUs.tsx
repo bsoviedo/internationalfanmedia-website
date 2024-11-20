2060c33a0adfcbe5a1bf19cba380bc79757f862e
bsoviedo <bsoviedoy@unal.edu.co>
2024-11-20 10:33:07 -0500

    add tiktok icon

--- app/Components-Home/AboutUs.tsx
+++ app/Components-Home/AboutUs.tsx
@@ -10,10 +10,10 @@ export default function AboutUs() {
             About Us
           </h2>
           <p className="font-medium text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-            It&lsquo;s one thing to see it and listen to it; it&lsquo;s another to live it.
-            We&lsquo;re bringing you as close to the Real Madrid experience as
-            possible, with exclusive content and the chance to actually live it
-            through our Road to El Bernabeu contest.
+            It&lsquo;s one thing to see it and listen to it; it&lsquo;s another
+            to live it. We&lsquo;re bringing you as close to the Real Madrid
+            experience as possible, with exclusive content and the chance to
+            actually live it through our Road to El Bernabeu contest.
           </p>
         </div>
 
@@ -24,80 +24,107 @@ export default function AboutUs() {
         </div>
 
         <div className="grid gap-8 mb-6 lg:mb-8 grid-cols-1 md:grid-cols-2">
-
-        <div className="flex items-center justify-around bg-gray-50 rounded-lg shadow sm:flex-row md:flex-row dark:bg-gray-800 dark:border-gray-700 md:h-64">
-        <a href="#">
-        <img
-            height={200}
-            width={200}
-            className="rounded-full"
-            src="/Picture1.png"
-            alt="Bonnie Avatar"
-        />
-    </a>
-    <div className="p-5">
-        <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <a href="#">Matthieu Estrada</a>
-        </h3>
-        <span className="text-gray-500 dark:text-gray-400">CEO & Founder</span>
-        <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"></p>
-        <ul className="flex space-x-4 sm:mt-0">
-            <li>
-                <a
+          <div className="flex items-center justify-around bg-gray-50 rounded-lg shadow sm:flex-row md:flex-row dark:bg-gray-800 dark:border-gray-700 md:h-64">
+            <a href="#">
+              <img
+                height={200}
+                width={200}
+                className="rounded-full"
+                src="/Picture1.png"
+                alt="Bonnie Avatar"
+              />
+            </a>
+            <div className="p-5">
+              <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#">Matthieu Estrada</a>
+              </h3>
+              <span className="text-gray-500 dark:text-gray-400">
+                CEO & Founder
+              </span>
+              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400"></p>
+              <ul className="flex space-x-4 sm:mt-0">
+                <li>
+                  <a
                     href="https://www.linkedin.com/in/matthieu-estrada-b74b801a7/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
+                  >
                     <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
                     >
-                        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                     </svg>
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://www.instagram.com/reminfanmedia/"
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/matuidibird/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
+                  >
                     <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
                     >
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                     </svg>
-                </a>
-            </li>
-            <li>
-                <a
+                  </a>
+                </li>
+                <li>
+                  <a
                     href="https://twitter.com/matuidibird"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-                >
+                  >
                     <svg
-                        className="w-5 h-5"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
                     >
-                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                      <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                     </svg>
-                </a>
-            </li>
-        </ul>
-    </div>
-</div>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.tiktok.com/@reminfanmedia"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                  >
+                    <div className="w-4 h-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        shapeRendering="geometricPrecision"
+                        textRendering="geometricPrecision"
+                        imageRendering="optimizeQuality"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        viewBox="0 0 449.45 515.38"
+                      >
+                        <path
+                          fillRule="nonzero"
+                          d="M382.31 103.3c-27.76-18.1-47.79-47.07-54.04-80.82-1.35-7.29-2.1-14.8-2.1-22.48h-88.6l-.15 355.09c-1.48 39.77-34.21 71.68-74.33 71.68-12.47 0-24.21-3.11-34.55-8.56-23.71-12.47-39.94-37.32-39.94-65.91 0-41.07 33.42-74.49 74.48-74.49 7.67 0 15.02 1.27 21.97 3.44V190.8c-7.2-.99-14.51-1.59-21.97-1.59C73.16 189.21 0 262.36 0 352.3c0 55.17 27.56 104 69.63 133.52 26.48 18.61 58.71 29.56 93.46 29.56 89.93 0 163.08-73.16 163.08-163.08V172.23c34.75 24.94 77.33 39.64 123.28 39.64v-88.61c-24.75 0-47.8-7.35-67.14-19.96z"
+                        />
+                      </svg>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
-<div className="flex items-center justify-around bg-gray-50 rounded-lg shadow sm:flex-row md:flex-row dark:bg-gray-800 dark:border-gray-700 md:h-64">
+          <div className="flex items-center justify-around bg-gray-50 rounded-lg shadow sm:flex-row md:flex-row dark:bg-gray-800 dark:border-gray-700 md:h-64">
             <a href="#">
               <img
                 height={150}
@@ -135,7 +162,7 @@ export default function AboutUs() {
                 </li>
                 <li>
                   <a
-                    href=""
+                    href="https://www.instagram.com/reminfanmedia/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -152,7 +179,7 @@ export default function AboutUs() {
                 </li>
                 <li>
                   <a
-                    href=""
+                    href="https://www.instagram.com/reminfanmedia/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -165,6 +192,32 @@ export default function AboutUs() {
                     >
                       <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                     </svg>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.tiktok.com/@reminfanmedia"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                  >
+                    <div className="w-4 h-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        shapeRendering="geometricPrecision"
+                        textRendering="geometricPrecision"
+                        imageRendering="optimizeQuality"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        viewBox="0 0 449.45 515.38"
+                      >
+                        <path
+                          fillRule="nonzero"
+                          d="M382.31 103.3c-27.76-18.1-47.79-47.07-54.04-80.82-1.35-7.29-2.1-14.8-2.1-22.48h-88.6l-.15 355.09c-1.48 39.77-34.21 71.68-74.33 71.68-12.47 0-24.21-3.11-34.55-8.56-23.71-12.47-39.94-37.32-39.94-65.91 0-41.07 33.42-74.49 74.48-74.49 7.67 0 15.02 1.27 21.97 3.44V190.8c-7.2-.99-14.51-1.59-21.97-1.59C73.16 189.21 0 262.36 0 352.3c0 55.17 27.56 104 69.63 133.52 26.48 18.61 58.71 29.56 93.46 29.56 89.93 0 163.08-73.16 163.08-163.08V172.23c34.75 24.94 77.33 39.64 123.28 39.64v-88.61c-24.75 0-47.8-7.35-67.14-19.96z"
+                        />
+                      </svg>
+                    </div>
                   </a>
                 </li>
               </ul>
